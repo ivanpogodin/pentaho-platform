db5c5b0081774de51adbbf728fc569883f651e69
PogI <ivan_pogodin@epam.com>
2016-07-01 18:27:55 +0400

    [BISERVER-13112] fix4 clean 3

--- extensions/test-src/org/pentaho/platform/web/http/api/resources/utils/EscapeUtilsTest.java
+++ extensions/test-src/org/pentaho/platform/web/http/api/resources/utils/EscapeUtilsTest.java
@@ -25,141 +25,68 @@ public class EscapeUtilsTest {
 
   @Test
   public void test0() {
-    System.out.println();
-    System.out.println( ( new Throwable() ).getStackTrace()[0].getMethodName() );
     final String src = "1";
     final String expect = "1";
-    //final String expect = "[\"as&lt;&gt;df\",\"&lt;xxx&gt;\"]";
-    Assert.assertEquals( "HTML", src, StringEscapeUtils.unescapeJava( expect ) );
+    //Check test data
+    Assert.assertEquals( "data", src, StringEscapeUtils.unescapeJava( expect ) );
 
     String actual = EscapeUtils.escapeJsonOrRaw( src );
-
-    System.out.println( "        src: " + src );
-    System.out.println( "     expect: " + expect );
-    System.out.println( "     actual: " + actual );
-
     Assert.assertEquals( expect, actual );
   }
 
   @Test
   public void test1() {
-    System.out.println();
-    System.out.println( ( new Throwable() ).getStackTrace()[0].getMethodName() );
     final String src = "[\"as<>df\",\"<xxx>\"]";
     final String expect = "[\"as\\u003C\\u003Edf\",\"\\u003Cxxx\\u003E\"]";
-    //final String expect = "[\"as&lt;&gt;df\",\"&lt;xxx&gt;\"]";
-    Assert.assertEquals( "JSON", JSON.toString( JSON.parse( src ) ), JSON.toString( JSON.parse( expect ) ) );
+    //Check test data
+    Assert.assertEquals( "data", JSON.toString( JSON.parse( src ) ), JSON.toString( JSON.parse( expect ) ) );
 
     String actual = EscapeUtils.escapeJsonOrRaw( src );
-
-    System.out.println( "        src: " + src );
-    System.out.println( "     expect: " + expect );
-    System.out.println( "     actual: " + actual );
-    System.out.println( "   src.json: " + JSON.toString( JSON.parse( src ) ) );
-    System.out.println( "expect.json: " + JSON.toString( JSON.parse( expect ) ) );
-    System.out.println( "actual.json: " + JSON.toString( JSON.parse( actual ) ) );
-
     Assert.assertEquals( expect, actual );
   }
 
   @Test
   public void test2() {
-    System.out.println();
-    System.out.println( ( new Throwable() ).getStackTrace()[0].getMethodName() );
     final String src = "[\"asdf\",123,\"<html>\", \"f&g\"]";
     final String expect = "[\"asdf\",123,\"\\u003Chtml\\u003E\",\"f\\u0026g\"]";
-    Assert.assertEquals( "JSON", JSON.toString( JSON.parse( src ) ), JSON.toString( JSON.parse( expect ) ) );
+    //Check test data
+    Assert.assertEquals( "data", JSON.toString( JSON.parse( src ) ), JSON.toString( JSON.parse( expect ) ) );
 
     String actual = EscapeUtils.escapeJsonOrRaw( src );
-
-    System.out.println( "        src: " + src );
-    System.out.println( "     expect: " + expect );
-    System.out.println( "     actual: " + actual );
-    System.out.println( "   src.json: " + JSON.toString( JSON.parse( src ) ) );
-    System.out.println( "expect.json: " + JSON.toString( JSON.parse( expect ) ) );
-    System.out.println( "actual.json: " + JSON.toString( JSON.parse( actual ) ) );
-
     Assert.assertEquals( expect, actual );
   }
 
   @Test
   public void test3() {
-    System.out.println();
-    System.out.println( ( new Throwable() ).getStackTrace()[0].getMethodName() );
     final String src = "{\"as&df\":\"<xxx>\", \"AS\" : \"zz\", \"X\":null}";
     final String expect = "{\"as\\u0026df\":\"\\u003Cxxx\\u003E\",\"AS\":\"zz\",\"X\":null}";
-    Assert.assertEquals( "JSON", JSON.toString( JSON.parse( src ) ), JSON.toString( JSON.parse( expect ) ) );
+    //Check test data
+    Assert.assertEquals( "data", JSON.toString( JSON.parse( src ) ), JSON.toString( JSON.parse( expect ) ) );
 
     String actual = EscapeUtils.escapeJsonOrRaw( src );
-
-    System.out.println( "        src: " + src );
-    System.out.println( "     expect: " + expect );
-    System.out.println( "     actual: " + actual );
-    System.out.println( "   src.json: " + JSON.toString( JSON.parse( src ) ) );
-    System.out.println( "expect.json: " + JSON.toString( JSON.parse( expect ) ) );
-    System.out.println( "actual.json: " + JSON.toString( JSON.parse( actual ) ) );
-
     Assert.assertEquals( expect, actual );
   }
 
   @Test
   public void test4() {
-    System.out.println();
-    System.out.println( ( new Throwable() ).getStackTrace()[0].getMethodName() );
     final String src = "{\"as&df\":\"<xxx>\", \"A\\\"S\" : \"z\\\"z\", \"X\":[123,\"\",\">\\\\<\"]}";
     final String expect =
         "{\"as\\u0026df\":\"\\u003Cxxx\\u003E\",\"A\\u0022S\":\"z\\u0022z\",\"X\":[123,\"\",\"\\u003E\\\\\\u003C\"]}";
-    Assert.assertEquals( "JSON", JSON.toString( JSON.parse( src ) ), JSON.toString( JSON.parse( expect ) ) );
+    //Check test data
+    Assert.assertEquals( "data", JSON.toString( JSON.parse( src ) ), JSON.toString( JSON.parse( expect ) ) );
 
     String actual = EscapeUtils.escapeJsonOrRaw( src );
-
-    System.out.println( "        src: " + src );
-    System.out.println( "     expect: " + expect );
-    System.out.println( "     actual: " + actual );
-    System.out.println( "   src.json: " + JSON.toString( JSON.parse( src ) ) );
-    System.out.println( "expect.json: " + JSON.toString( JSON.parse( expect ) ) );
-    System.out.println( "actual.json: " + JSON.toString( JSON.parse( actual ) ) );
-
     Assert.assertEquals( expect, actual );
   }
 
   @Test
-  public void test10() {
-    System.out.println();
-    System.out.println( ( new Throwable() ).getStackTrace()[0].getMethodName() );
-    final String src = "1";
-    final String expect = "1";
-    Assert.assertEquals( "JSON", JSON.toString( JSON.parse( src ) ), JSON.toString( JSON.parse( expect ) ) );
-
-    String actual = EscapeUtils.escapeJsonOrRaw( src );
-
-    System.out.println( "        src: " + src );
-    System.out.println( "     expect: " + expect );
-    System.out.println( "     actual: " + actual );
-    System.out.println( "   src.json: " + JSON.toString( JSON.parse( src ) ) );
-    System.out.println( "expect.json: " + JSON.toString( JSON.parse( expect ) ) );
-    System.out.println( "actual.json: " + JSON.toString( JSON.parse( actual ) ) );
-
-    Assert.assertEquals( expect, actual );
-  }
-
-  @Test
-  public void test11() {
-    System.out.println();
-    System.out.println( ( new Throwable() ).getStackTrace()[0].getMethodName() );
+  public void test5() {
     final String src = "{as&df<html>\"\\123";
     final String expect = "{as\\u0026df\\u003Chtml\\u003E\\u0022\\\\123";
-    System.out.println( "                src: " + src );
-    System.out.println( "             expect: " + expect );
-    System.out.println( "expect.unescapeHtml: " + StringEscapeUtils.unescapeHtml( expect ) );
-    Assert.assertEquals( "HTML", src, StringEscapeUtils.unescapeJava( expect ) );
+    //Check test data
+    Assert.assertEquals( "data", src, StringEscapeUtils.unescapeJava( expect ) );
 
     String actual = EscapeUtils.escapeJsonOrRaw( src );
-
-    System.out.println( "        src: " + src );
-    System.out.println( "     expect: " + expect );
-    System.out.println( "     actual: " + actual );
-
     Assert.assertEquals( expect, actual );
   }
 
